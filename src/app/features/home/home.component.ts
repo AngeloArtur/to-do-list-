25b89d9ebfa5f3280659b7bd097f53a9744f319a
Ângelo Artur <ang.vianna@gmail.com>
2024-07-03 19:42:20 -0300

    feat: delete Items from localSotrage

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,25 +1,33 @@
 import { Component, signal } from '@angular/core';
 import { InputAreaComponent } from './components/input-area/input-area.component';
 import { IListItens } from '../../core/interface/IListItens.interface';
+import { InputListItemComponent } from './components/input-list-item/input-list-item.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InputAreaComponent],
+  imports: [InputAreaComponent, InputListItemComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   public addItem:boolean = true;
-  private setListenItens = signal<IListItens[]>([this.parseItens()]);
-  getListItens = this.setListenItens.asReadonly();
+  private setListenItens = signal<IListItens[]>(this.parseItens());
+  public getListItems = this.setListenItens.asReadonly();
 
   private parseItens(){
     return JSON.parse(localStorage.getItem('@my-list') || '[]')
   }
 
-  public getInputItens(value:IListItens) {
-    localStorage.setItem('@my-list', JSON.stringify([value]))
+  public getInputAddItems(value:IListItens) {
+    localStorage.setItem('@my-list', JSON.stringify([...this.setListenItens(),value]));
     console.log(value)
+
+    return this.setListenItens.set(this.parseItens())
+  }
+
+  public deleteAllItems() {
+    localStorage.clear();
+    return this.setListenItens.set(this.parseItens());
   }
 }
